91e87647cd906b8d5fcfd17e8f0e496e1b5058d5
CGTI9 <cgti9@CGTI9s-MacBook-Pro.local>
2022-11-16 22:43:42 -0700

    Day 20-21 Guess the flag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by CGTI9 on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
