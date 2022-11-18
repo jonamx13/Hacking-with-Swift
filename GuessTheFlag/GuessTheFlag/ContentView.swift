d0592aa11196034577edc5a047f52a461c12f5e4
CGTI9 <cgti9@CGTI9s-MacBook-Pro.local>
2022-11-17 23:34:42 -0700

    Day 22 Guess the flag (GOT Version) add finishing, alerts and commenting

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingScore = false
-    @State private var scoreWrong = 0
-    @State private var scoreCorrect = 0
-    @State private var scoreTitle = ""
-    @State private var houses = ["Arryn", "Baratheon", "Greyjoy", "Lannister", "Martell", "Stark", "Targaryen", "Tully", "Tyrell"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
+    //Score variables
+    @State private var showingScore: Bool = false
+    @State private var scoreWrong: Int = 0
+    @State private var scoreCorrect: Int = 0
     
-    var gold = #colorLiteral(red:0.9882352941, green: 0.7607843137, blue: 0, alpha: 1)
+    //Structure game variables
+    @State private var scoreTitle: String = ""
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var houses: [String] = ["Arryn", "Baratheon", "Greyjoy", "Lannister", "Martell", "Stark", "Targaryen", "Tully", "Tyrell"].shuffled()
+    
+    //Game life-cycle variables
+    @State private var gameFinish: Bool = false
+    private var gameLenght: Int = 8
     
     var body: some View {
         ZStack {
@@ -23,7 +28,7 @@ struct ContentView: View {
                 Image("background")
                     .resizable()
                     .mask(LinearGradient(gradient: Gradient(colors: [.clear, .black, .black, .clear]), startPoint: .top, endPoint: .bottom))
-            }
+            } // Background
             
             
             VStack {
@@ -32,6 +37,7 @@ struct ContentView: View {
                     .frame(maxHeight: 40)
                     .mask(Text("Guess The House")
                         .font(.custom("Game of Thrones", size: 32)))
+                // Title
                 
                 VStack(spacing: 20) {
                     VStack {
@@ -40,7 +46,7 @@ struct ContentView: View {
                             .font(.custom("TrajanPro-Bold", size: 18))
                         Text(houses[correctAnswer])
                             .font(.custom("Game of Thrones", size: 25))
-                    }
+                    } // Instructions
                     
                     ForEach(0..<3) { number in
                         Button {
@@ -51,13 +57,15 @@ struct ContentView: View {
                                 .clipShape(Capsule())
                                 .shadow(radius: 5)
                         }
-                    }
+                    } // House flags buttons
                 }
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, 20)
                 .background(.ultraThinMaterial.opacity(0.7))
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 .shadow(radius: 10)
+                
+                // Game UI
                 
                 Spacer()
                 Spacer()
@@ -69,36 +77,55 @@ struct ContentView: View {
                     .foregroundColor(.white)
                     .font(.custom("TrajanPro-Bold", size: 30))
                 
+                // Scores
                 Spacer()
             }
             .padding()
+            // Game screen
         }
+        // Alert screens
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", role: .cancel, action: askQuestion)
             Button("Drakarys", role: .destructive, action: resetScore)
         } message: {
             Text("Your score is \(scoreCorrect)")
         }
-        .environment(\.colorScheme, .dark)
-    }
-    
-    func flagTapped(_ number: Int) {
-        if number == correctAnswer {
-            scoreCorrect += 1
-            scoreTitle = "Correct"
-        } else {
-            scoreWrong += 1
-            scoreTitle = "Wrong"
+        .alert("""
+            Game finished
+            your score was:
+            Correct: \(scoreCorrect)
+            Wrong: \(scoreWrong)
+            """, isPresented: $gameFinish) {
+            Button("Drakarys", role: .none, action: resetScore)
+                .foregroundColor(.red)
+
         }
-        showingScore = true
-    }
+        .environment(\.colorScheme, .dark)
+        // Body
+    } // ContentView
     
-    func askQuestion() {
+func flagTapped(_ number: Int) {
+    if number == correctAnswer {
+        scoreCorrect += 1
+        scoreTitle = "Correct"
+    } else {
+        scoreWrong += 1
+        scoreTitle = "Wrong, you chose the \(houses[number]) house"
+    }
+        
+        if (scoreCorrect + scoreWrong) == gameLenght {
+            gameFinish = true
+        }
+        
+    showingScore = true
+}
+    
+func askQuestion() {
         houses.shuffle()
         correctAnswer = Int.random(in: 0...2)
-    }
+}
     
-    func resetScore() {
+func resetScore() {
         houses.shuffle()
         correctAnswer = Int.random(in: 0...2)
         scoreCorrect = 0
@@ -106,6 +133,7 @@ struct ContentView: View {
     }
 }
 
+// Ending
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
