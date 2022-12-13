ac90f615b2a01d9962b6200e148a0d1e28e25a58
CGTI9 <cgti9@CGTI9s-MacBook-Pro.local>
2022-12-12 18:46:11 -0600

    Day 31 (Add score and rules to don't allow adding words with less than 3 chars andtitle word itself)

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -12,37 +12,77 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    @State private var score = 0
+    @State private var lastWord = ""
+    
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var showingError = false
     
     var body: some View {
-        NavigationView {
-            List {
-                // Enter word
-                Section {
-                    TextField("Enter your word", text: $newWord)
-                        .autocapitalization(.none)
-                }
-                // Valid words
-                Section {
-                    ForEach(usedWords, id: \.self) { word in
-                        HStack {
-                            Image(systemName: "\(word.count).circle")
-                            Text(word)
+        ZStack {
+            NavigationView {
+                List {
+                    // Enter word
+                    Section {
+                        TextField("Enter your word", text: $newWord)
+                            .autocapitalization(.none)
+                    }
+                    // Valid words
+                    Section {
+                        ForEach(usedWords, id: \.self) { word in
+                            HStack {
+                                Image(systemName: "\(word.count).circle")
+                                Text(word)
+                            }
                         }
                     }
                 }
-            }
-            .navigationTitle(rootWord)
-            .onSubmit(addNewWord)
-            .onAppear(perform: startGame)
-            .alert(errorTitle, isPresented: $showingError) {
-                Button("OK", role: .cancel) { }
-            } message: {
-                Text(errorMessage)
-            }
-        }
+                .navigationTitle(rootWord)
+                .onSubmit(addNewWord)
+                .onAppear(perform: startGame)
+                .alert(errorTitle, isPresented: $showingError) {
+                    Button("OK", role: .cancel) { }
+                } message: {
+                    Text(errorMessage)
+                }
+                .toolbar {
+                    HStack {
+                        Button("Restart") {
+                            startGame()
+                            newWord = ""
+                            lastWord = ""
+                            usedWords = []
+                            score = 0
+                        }
+                        Button("Next") {
+                            startGame()
+                            newWord = ""
+                        }
+                    }
+                }
+            } // NavigationView
+            //Score
+            VStack {
+                Spacer()
+                Section {
+                    VStack {
+                        Text("Score:")
+                            .font(.largeTitle)
+                        Text("\(score)")
+                            .font(.title2)
+                            .foregroundColor(.blue)
+                        Text("Last word:")
+                            .font(.largeTitle)
+                        Text(lastWordMessage())
+                            .font(.title2)
+                            .foregroundColor(.blue)
+                    }
+                    .frame(minWidth: 100, maxWidth: .infinity, minHeight: 50, maxHeight: 150)
+                    .background(.thinMaterial)
+                }
+            } // VStack Score
+        } // ZStack
     } // View
     
     func addNewWord() {
@@ -59,6 +99,14 @@ struct ContentView: View {
             return
         }
         
+        guard isAllowed(word: answer) else {
+            let notAllowedTitle = answer.count <= 3 ? "Not long enough" : "Same word as title"
+            let notAllowedMessage = answer.count <= 3 ? "You have to use at least 4 letters" : "Title itself is not allowed"
+            
+            wordError(title: notAllowedTitle , message: notAllowedMessage)
+            return
+        }
+        
         guard isReal(word: answer) else {
             wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
             return
@@ -67,7 +115,9 @@ struct ContentView: View {
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
+        score += 1
         newWord = ""
+        lastWord = answer
     }
     
     func startGame() {
@@ -98,6 +148,14 @@ struct ContentView: View {
         return true
     }
     
+    func isAllowed(word: String) -> Bool {
+        if word.count <= 3 || word == rootWord {
+            return false
+        } else {
+            return true
+        }
+    }
+    
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
@@ -110,6 +168,13 @@ struct ContentView: View {
         errorTitle = title
         errorMessage = message
         showingError = true
+    }
+    func lastWordMessage() -> String {
+        if lastWord != "" {
+            return "'\(lastWord)' using \(lastWord.count) letters"
+        } else {
+            return ""
+        }
     }
     //ContentView
 }
